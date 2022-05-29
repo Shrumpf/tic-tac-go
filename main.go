00696d974d8f4ac068a1e32938dc7a75b8a3aac6
Shrumpf <flori.voss@gmx.net>
2022-05-29 14:19:56 +0000

    Basic Logic Player vs. Player

--- main.go
+++ main.go
@@ -1,5 +1,72 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	/*
+		|x|x|x|
+		|-|o|-|
+		|-|-|x|
+	*/
+	won := false
+	ls := [9]string{"", "", "", "", "", "", "", "", ""}
+	var input int
+	currentPlayer := "x"
+
+	for !won {
+		fmt.Scan(&input)
+		if input >= 0 && input <= 8 {
+			ls[input] = currentPlayer
+
+			if currentPlayer == "x" {
+				currentPlayer = "o"
+			} else {
+				currentPlayer = "x"
+			}
+
+			winner := checkWin(ls)
+
+			if len(winner) > 0 {
+				won = true
+				fmt.Println("Gz", winner)
+			}
+
+			printBoard(ls)
+		}
+	}
+}
+
+func checkWin(ls [9]string) string {
+	checks := [][]string{
+		ls[0:3], ls[3:6], ls[6:9],
+		{ls[0], ls[3], ls[6]},
+		{ls[1], ls[4], ls[7]},
+		{ls[2], ls[5], ls[8]},
+		{ls[0], ls[4], ls[8]},
+		{ls[2], ls[4], ls[6]},
+	}
+
+	for i := 0; i < len(checks); i++ {
+		if allEqual(checks[i]) {
+			return checks[i][0]
+		}
+	}
+
+	return ""
+}
+
+func allEqual(a []string) bool {
+	for i := 1; i < len(a); i++ {
+		if a[i] != a[0] || a[i] == "" {
+			return false
+		}
+	}
+	return true
+}
+
+func printBoard(ls [9]string) {
+	fmt.Println("")
+	fmt.Println(ls[0:3])
+	fmt.Println(ls[3:6])
+	fmt.Println(ls[6:9])
 }
